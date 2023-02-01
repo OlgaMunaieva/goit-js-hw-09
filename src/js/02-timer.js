8bac9e89436d1082c44a7fc1d41a7d3f1d4ae291
OlgaMunaieva <asyssync@gmail.com>
2023-02-01 20:16:47 +0200

    start task-2

---
+++ src/js/02-timer.js
@@ -0,0 +1,22 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const buttonStart = document.querySelector('[data-start]');
+buttonStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', { options });
+
+console.log(options.defaultDate);
+console.log(options);
