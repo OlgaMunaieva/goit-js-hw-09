957109424783a1ba94745f739fa8deb8bc994800
OlgaMunaieva <asyssync@gmail.com>
2023-02-01 18:28:42 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,4 +3,25 @@ function getRandomHexColor() {
 }
 
 const buttonStart = document.querySelector('[data-start]');
-console.log(buttonStart);
+const buttonStop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let timerId = null;
+
+/**
+ *by clicking on the start button makes it inactive, and the stop button is active, launches the function of randomly obtaining a color and sets it to the body
+ */
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  timerId = setInterval(
+    () => (bodyEl.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+});
+
+buttonStop.addEventListener('click', () => {
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+  clearInterval(timerId);
+});
