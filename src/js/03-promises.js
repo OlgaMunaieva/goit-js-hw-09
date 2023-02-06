c489ced22011eaddb9f48042d67314dc1f319674
OlgaMunaieva <asyssync@gmail.com>
2023-02-06 12:55:50 +0200

    finish task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,31 @@
 import Notiflix from 'notiflix';
 
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  for (let i = 1; i <= amount.value; i++) {
+    const positionDelay = Number(delay.value) + Number(step.value) * (i - 1);
+    createPromise(i, positionDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+  // event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
@@ -12,21 +38,3 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-const btnCreate = document.querySelector('button[type="submit"]');
-// console.log(btnCreate);
-
-btnCreate.addEventListener('submit', handleButton);
-
-function handleButton(event) {
-  event.preventDefault();
-  event.console.log(event.target);
-}
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
